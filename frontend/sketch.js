acb45f51a25fb22a298452f15ff4ab82e4d6f93a
Aditya Chatterjee <adityachatterjee42@gmail.com>
2018-11-14 20:48:17 -0800

    no more borders

--- frontend/sketch.js
+++ frontend/sketch.js
@@ -2,14 +2,14 @@ var bubAr=[];
 
 function setup() {
   createCanvas((window.innerWidth || document.body.clientWidth) - 4, (window.innerHeight || document.body.clientHeight) - 4);
+  smooth();
 }
 
 function draw() {
   // put drawing code here
   background(0, 0, 0);
   bubAr.forEach(function(bub) {
-    bub.move();
-    bub.display();
+    bub.action();
   });
 }
 
@@ -17,16 +17,22 @@ function Bubble() {
   this.x = random(width);
   this.y = random(height);
   this.diameter = random(50, 70);
-  this.speed = 2;
+  this.randomspeed = 1;
 
-  this.move = function() {
-    this.x += random(-this.speed, this.speed);
-    this.y += random(-this.speed, this.speed);
+  this.randomMove = function() {
+    this.x += random(-1, 1);
+    this.y += random(-1, 1);
   };
 
   this.display = function() {
     ellipse(this.x, this.y, this.diameter, this.diameter);
   };
+
+  this.action = function() {
+    strokeWeight(0);
+    this.randomMove();
+    this.display();
+  }
 }
 
 function mouseClicked() {
